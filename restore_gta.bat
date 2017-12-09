0f08e2c881af160bb8fc3a13910609c97ce00d77
DREAMS <knuve@gmx.de>
2017-12-09 17:17:36 +0100

    added bat file

---
+++ restore_gta.bat
@@ -0,0 +1,10 @@
+@echo off
+echo.
+echo [System]Directing to your GTA Files
+echo.
+cd D:\Programme\Rockstar\ :: your Path to gta directory
+echo [System]Successfully directed
+echo.
+echo [System]starting verifying Progress
+GTAVLauncher.exe -verify
+pause
